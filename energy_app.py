2cfc43a550ee1ecccddbf93a333a0985e399d6ce
Rouven <rouven.pampus@gmail.com>
2024-07-09 15:25:34 +0200

    first prototype

--- energy_app.py
+++ energy_app.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from packages.db_utils import get_data_from_db_st, get_engine
-from packages.streamlit_app import classify_timestamp
+from packages.streamlit_app import get_timeframe
 import plotly.express as px
 import plotly.graph_objects as go
+from datetime import datetime,timedelta
 
 st.set_page_config(
     page_title="Ex-stream-ly Cool App",
@@ -18,12 +19,10 @@ st.set_page_config(
     }
 )
 
-#Example text
-st.title('Our amazing, world-changing app :sunglasses:')
+################## data extraction ##################
 
 #Place for queries
-query_prices = """
-    SELECT * 
+query_prices = """ SELECT timestamp, de_lu as price, unit
     FROM "02_silver".fact_day_ahead_prices_germany
     WHERE date_trunc('day', "timestamp") >= (
         SELECT MAX(date_trunc('day', "timestamp")) - INTERVAL '7 days'
@@ -33,48 +32,95 @@ query_prices = """
 #Load price data
 df_prices = get_data_from_db_st(query_prices)
 
+################## data transformation ##################
+
 #Do transformations to price dataframe
-df_prices["timestamp"] = df_prices["timestamp"].dt.tz_convert("Europe/Berlin")
-df_prices['timeframe'] = df_prices['timestamp'].apply(classify_timestamp)
-df_prices["hour"] = df_prices.timestamp.dt.strftime('%H:%M')
-df_prices
+df_prices["timestamp"] = df_prices["timestamp"].dt.tz_convert("Europe/Berlin") #timezone
+df_prices["hour"] = df_prices.timestamp.dt.strftime('%H:%M') #add hour column
+df_prices['date'] = df_prices.timestamp.dt.strftime('%Y-%m-%d') #add date column
+df_prices["timeframe"] = df_prices["timestamp"].apply(get_timeframe)
 
-timeframe_list = df_prices.timeframe.unique()
+#get timefram entries
+timeframe_entries = df_prices.timeframe.unique()
 
-now = pd.Timestamp.now()
+#Add current time metrics
+timestamp_now = pd.Timestamp.now()
+datetime_now = timestamp_now.floor('s')
+time_now = timestamp_now.strftime('%H:%M')
+hour_now = timestamp_now.floor('H').strftime('%H:%M')
+hour_before = format(F"{(datetime.now() - timedelta(hours=1)).hour}:00")
+today_date = datetime_now.strftime('%Y-%m-%d')
 
-current_price = df_prices[df_prices["timestamp"].dt.hour == pd.Timestamp.now().hour].max().de_lu
-current_price
+#Add current price metrics
+current_price = df_prices.query("hour == @hour_now and date == @today_date").price.values[0]
+before_price = df_prices.query("hour == @hour_before and date == @today_date").price.values[0]
+price_delta = round((current_price - before_price),2)
 
-#Metric bar
-col1, col2, col3 = st.columns(3)
+################## definition of charts ##################
+
+def create_bar_chart(x_data, y_data, title, x_title, y_title):
+    """
+    Create a bar chart using Plotly.
+
+    Parameters:
+    x_data (list): Data for the x-axis.
+    y_data (list): Data for the y-axis.
+    title (str): Title of the bar chart.
+    x_title (str): Label for the x-axis.
+    y_title (str): Label for the y-axis.
+
+    Returns:
+    fig: A Plotly figure object representing the bar chart.
+    """
+    fig = go.Figure()
+
+    # Add bar trace
+    fig.add_trace(go.Bar(
+        x=x_data,
+        y=y_data,
+        marker=dict(
+        color=y_data,  # Use y_data for coloring
+        colorscale='Blues'
+    )
+    ))
+    
+   
+    # Update layout
+    fig.update_layout(
+        title=title,
+        xaxis_title=x_title,
+        yaxis_title=y_title,
+        
+    )
+    return fig
+
+################## Design elements ##################
+
+#Example text
+st.title('Our amazing, world-changing app :sunglasses:')
+
+col1, col2, col3 = st.columns(spec=3, gap="large")
 with col1:
-    st.metric("current time", now.strftime('%H:%M'))
+    st.metric("Date", today_date)
 with col2:
-    st.metric("current price", current_price)
+    st.metric("Time", time_now)
+
+    
+col1, col2, col3 = st.columns(spec=3, gap="large")
+with col1 :
+    timeframe_radio = st.selectbox("Daily prices for:", timeframe_entries, index=2)
+with col2:
+    unit_radio = st.selectbox("select unit",["€/MWh","ct€/KWh"])
+    if unit_radio == "€/MWh":
+        x = 1
+    elif unit_radio == "ct€/KWh":
+        x = 10          
 with col3:
-    st.metric("current time", now.strftime('%H:%M'))
+    st.metric(label ="Current price", value = f"{round(current_price/x,2)} {unit_radio}", delta= f"{round(price_delta/x,2)} {unit_radio}", delta_color="inverse")
 
-timeframe_radio = st.selectbox("Daily prices for:", timeframe_list, )
-    
-df = df_prices.query('timeframe == @timeframe_radio')
-    
-# Create stepped line chart using Plotly
-fig = go.Figure()
+df_sel = df_prices.query('timeframe == @timeframe_radio')
 
-fig.add_trace(go.Scatter(
-    x=df['timestamp'],
-    y=df['de_lu'],
-    mode='lines',
-    line_shape='hv'
-))
+# Creating and calling the price bar chart
+fig = create_bar_chart(df_sel["timestamp"], df_sel["price"]/x, title="Day-ahead-price", x_title="Hour", y_title=unit_radio)
 
-# Update layout
-fig.update_layout(
-    title='Stepped Line Chart',
-    xaxis_title='Timestamp',
-    yaxis_title='EUR/MWh'        
-)
-
-# You can call any Streamlit command, including custom components:
 st.plotly_chart(fig, theme="streamlit", use_container_width=True)
--- packages/streamlit_app.py
+++ packages/streamlit_app.py
@@ -1,25 +1,18 @@
+import pandas as pd
 from datetime import datetime, timedelta
+import pytz
 
-def classify_timestamp(ts):
-    """function to create column for timestamp classification into 
-    historic, today, tomorrow, future for easy filtering in app.
-
-    Args:
-        None
-
-    Returns:
-        str: historic, today, tomorrow, future
-    """
-    now = datetime.now(ts.tz)
-    today_start = now.replace(hour=0, minute=0, second=0, microsecond=0)
-    tomorrow_start = today_start + timedelta(days=1)
-    day_after_tomorrow_start = tomorrow_start + timedelta(days=1)
-    
-    if ts < today_start:
-        return "historic"
-    elif today_start <= ts < tomorrow_start:
-        return "today"
-    elif tomorrow_start <= ts < day_after_tomorrow_start:
-        return "tomorrow"
+def get_timeframe(ts):
+    today = pd.to_datetime(datetime.now().date())
+    tomorrow = today + timedelta(days=1)
+    yesterday = today - timedelta(days=1)
+    if ts.date() == today.date():
+        return 'today'
+    elif ts.date() == tomorrow.date():
+        return 'tomorrow'
+    elif ts.date() == yesterday.date():
+        return 'yesterday'
+    elif ts.date() < yesterday.date():
+        return 'history'
     else:
-        return "future"
+        return 'other'
